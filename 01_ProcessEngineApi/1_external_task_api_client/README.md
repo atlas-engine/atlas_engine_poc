c136d7ffd6d8e19fe73839381a10405c6d5a3137
ElRaptorus <christian.werner@5minds.de>
2019-05-15 15:39:33 +0200

    :fire: Remove separated ExternalTaskApiClient

--- 01_ProcessEngineApi/1_external_task_api_client/README.md
+++
@@ -1,1 +0,0 @@
-# external_task_api_client
--- 01_ProcessEngineApi/1_external_task_api_client/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './external_task_api_http_client';
-export * from './external_task_api_internal_client';
-export * from './external_task_worker';
